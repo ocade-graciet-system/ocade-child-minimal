84fd4428ccb76845a8692f1fee8ab57792a9da48
valentin charrier <valentin.charrier@htomail.fr>
2023-09-25 14:06:43 +0200

    Activation des commentaires tout le temps mais caché dans la bar admin

--- functions.php
+++ functions.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace OcadeChild;
 
@@ -7,28 +7,50 @@ function enqueue_front_styles() {
   $stylesheet_dir = get_stylesheet_directory();
   $stylesheet_uri = get_stylesheet_directory_uri();
 
-  if (file_exists($stylesheet_dir . '/styles/css/main.min.css')) { 
-    wp_enqueue_style( 'front-style-main', $stylesheet_uri . '/styles/css/main.min.css');
+  if (file_exists($stylesheet_dir . '/styles/css/main.min.css')) {
+    wp_enqueue_style('front-style-main', $stylesheet_uri . '/styles/css/main.min.css');
   }
-  if (file_exists($stylesheet_dir . '/fonts/fonts.css')) { 
-    wp_enqueue_style( 'front-fonts', $stylesheet_uri . '/fonts/fonts.css'); /** Pas minifié */
+  if (file_exists($stylesheet_dir . '/fonts/fonts.css')) {
+    wp_enqueue_style('front-fonts', $stylesheet_uri . '/fonts/fonts.css');
+    /** Pas minifié */
   }
 }
-add_action( 'wp_enqueue_scripts', 'OcadeChild\enqueue_front_styles' );
+add_action('wp_enqueue_scripts', 'OcadeChild\enqueue_front_styles');
 
 /** Charger du CSS dans l'editeur gutenberg en prenant le nom des fichiers générer dans le dossier css de ocade-child */
 function enqueue_editor_styles() {
   $stylesheet_dir = get_stylesheet_directory();
   $stylesheet_uri = get_stylesheet_directory_uri();
 
-  if (file_exists($stylesheet_dir . '/styles/css/main-editor.min.css')) {  /** main-editor.min.css est un  */
-    wp_enqueue_style( 'editor-style-main', $stylesheet_uri . '/styles/css/main-editor.min.css');
+  if (file_exists($stylesheet_dir . '/styles/css/main-editor.min.css')) {
+    /** main-editor.min.css est un  */
+    wp_enqueue_style('editor-style-main', $stylesheet_uri . '/styles/css/main-editor.min.css');
   }
-  if (file_exists($stylesheet_dir . '/fonts/fonts.css')) { 
-    wp_enqueue_style( 'editor-fonts', $stylesheet_uri . '/fonts/fonts.css'); /** Pas minifié */
+  if (file_exists($stylesheet_dir . '/fonts/fonts.css')) {
+    wp_enqueue_style('editor-fonts', $stylesheet_uri . '/fonts/fonts.css');
+    /** Pas minifié */
   }
 }
-add_action( 'enqueue_block_editor_assets', 'OcadeChild\enqueue_editor_styles' );
+add_action('enqueue_block_editor_assets', 'OcadeChild\enqueue_editor_styles');
 
-/** Suppression des commentaires dans WordPress */
-// require_once( get_stylesheet_directory() . '/includes/functions/RemoveComments.php' );
+
+/** Cacher menus dans l'admin bar du back-office */
+function custom_admin_css() {
+  echo '<style>
+  /** Cache le menu commentaires */
+  #menu-comments {
+    display: none;
+  }
+  
+  /** Réglages Kadence blocks */
+  #toplevel_page_kadence-blocks {
+    display: none;
+  }
+
+  /** Loco Translate */
+  #toplevel_page_loco {
+    display: none;
+  }
+  </style>';
+}
+add_action('admin_head', 'OcadeChild\custom_admin_css');
